cc6d4c63666244b6a4a15e58096cd0ccb8e420be
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-05-09 14:14:04 +0200

    Add NextDay info.

--- src/components/NextDay.vue
+++ src/components/NextDay.vue
@@ -7,7 +7,13 @@
     <div class="div">
       {{ day.name }}
     </div>
-    <!-- {{d.data.main.temp}} -->
+    
+    <div class="info">
+      <p>{{day.data.main.temp}}&ordm;C</p>
+      <p>{{day.data.wind.speed}} km/h</p>
+      <p>{{day.data.weather[0].description}}</p>
+      <p>{{day.data.main.pressure}} hPa</p>
+    </div>
   </div>
 </template>
 
@@ -24,6 +30,25 @@ export default {
   margin-bottom: 5px;
   border-right: 1px solid #d1ab13;
   font-size: 0.7rem;
+  position: relative;
+  cursor: pointer;
+}
+.info {
+  position: absolute;
+  top: 200%;
+  left: 0;
+  right: 0;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.9);
+  padding: 10px;
+  opacity: 0;
+  transition: all 0.6s ease-in-out;
+  z-index: 3;
+  border-radius: 2px;
+}
+.next-day:hover > .info {
+  opacity: 1;
+  top: -200%;
 }
 .next-day:last-of-type {
   border: 0;
